cff8d0c9ca17d6ec25db87d6bd4101a139e6175b
julius ndakala <juliusndakala35@gmail.com>
2023-08-02 10:40:11 +0300

    updated the footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,145 +1,56 @@
-import React from 'react';
-import styled from 'styled-components';
-import FooterCol from './FooterCol';
-import PText from './PText';
+import React from "react";
+import "./Footer.css";
+import PrivacyModal from "./ModalView/PrivacyModal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faYoutube,
+    faGithub,
+    faFacebook,
+} from "@fortawesome/free-brands-svg-icons";
+import { faCopyright } from "@fortawesome/free-solid-svg-icons";
 
-const FooterStyle = styled.div`
-  background-color: var(--deep-dark);
-  padding-top: 10rem;
-  .container {
-    display: flex;
-    gap: 3rem;
-  }
-  .footer__col1 {
-    flex: 2;
-  }
-  .footer__col2,
-  .footer__col3,
-  .footer__col4 {
-    flex: 1;
-  }
-  .footer__col1__title {
-    font-size: 3.5rem;
-    margin-bottom: 1rem;
-  }
-  .copyright {
-    background-color: var(--dark-bg);
-    text-align: left;
-    padding: 1rem 0;
-    margin-top: 5rem;
-    .para {
-      margin-left: 0;
-    }
-  }
-  @media only screen and (max-width: 768px) {
-    .container {
-      flex-direction: column;
-      gap: 0rem;
-      & > div {
-        margin-top: 5rem;
-      }
-    }
-    .footer__col1 .para {
-      max-width: 100%;
-    }
-    .copyright {
-      .container {
-        div {
-          margin-top: 0;
-        }
-      }
-    }
-  }
-`;
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-container">
+                <div className="item1">
+                    <PrivacyModal />
+                </div>
 
-export default function Footer() {
-  return (
-    <FooterStyle>
-      <div className="container">
-        <div className="footer__col1">
-          <h1 className="footer__col1__title">Ayan Khan</h1>
-          <PText>
-            A freelance web designer and developer from Chittagong, Bangladesh.
-            I always make websites that have unique designs and also has a good
-            performance rate.
-          </PText>
-        </div>
-        <div className="footer__col2">
-          <FooterCol
-            heading="Important Links"
-            links={[
-              {
-                title: 'Home',
-                path: '/',
-                type: 'Link',
-              },
-              {
-                type: 'Link',
-                title: 'About',
-                path: '/about',
-              },
-              {
-                type: 'Link',
-                title: 'Projects',
-                path: '/projects',
-              },
-              {
-                type: 'Link',
-                title: 'Contact',
-                path: '/contact',
-              },
-            ]}
-          />
-        </div>
-        <div className="footer__col3">
-          <FooterCol
-            heading="Contact Info"
-            links={[
-              {
-                title: '+88012312',
-                path: 'tel:+88012312',
-              },
-              {
-                title: 'webcifar@gmail.com',
-                path: 'mailto:webcifar@gmail.com',
-              },
-              {
-                title: 'GEC Circle, Chittagong, Bangladesh',
-                path: 'http://google.com/maps',
-              },
-            ]}
-          />
-        </div>
-        <div className="footer__col4">
-          <FooterCol
-            heading="social Links"
-            links={[
-              {
-                title: 'Facebook',
-                path: 'http://facebook.com',
-              },
-              {
-                title: 'Twitter',
-                path: 'http://twitter.com',
-              },
-              {
-                title: 'Instagram',
-                path: 'http://instagram.com',
-              },
-            ]}
-          />
-        </div>
-      </div>
-      <div className="copyright">
-        <div className="container">
-          <PText>
-            © 2021 - Ayan Khan | Designed By{' '}
-            <a target="_blank" rel="noreferrer" href="http://webcifar.com">
-              web cifar
-            </a>{' '}
-          </PText>
-        </div>
-      </div>
-    </FooterStyle>
-  );
-}
+                <div className="item2">
+                    <span style={{ paddingRight: 5 }}>Copyright </span>
+                    <FontAwesomeIcon icon={faCopyright} />{" "}
+                    <span style={{ paddingLeft: 5 }}>
+                        {new Date().getFullYear()} YourCompany. All Rights
+                        Reserved.
+                    </span>
+                </div>
+                <a
+                    href="https://github.com/sudiptob2/simple-react-footer"
+                    target="_blank"
+                    className="item3"
+                >
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <a
+                    href="http://fb.com/sudiptob2"
+                    target="_blank"
+                    className="item4"
+                >
+                    <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                <a
+                    href="https://www.youtube.com/"
+                    target="_blank"
+                    className="item5"
+                >
+                    <FontAwesomeIcon icon={faYoutube} />
+                </a>
+
+                {false && <PrivacyModal click={true} />}
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
